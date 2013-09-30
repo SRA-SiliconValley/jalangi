e33acfd70c369122435d3c9da77cd31514e18f60
Simon Jensen <s.jensen@samsung.com>
2013-09-30 12:16:06 -0700

    Run configurations for integer12

---
+++ jalangi/config/gen-integer12-new.sh
@@ -0,0 +1,7 @@
+#Mon Sep 30 11:08:24 PDT 2013
+parameterstoprog=
+mainfile=tests/unit/integer12
+parameters=-i 5
+workingdirectory=/home/simonhj/src/jalangi
+resources=
+analysis=concolic
---
+++ jalangi/config/run-tests-integer12-new.sh
@@ -0,0 +1,7 @@
+#Mon Sep 30 11:08:24 PDT 2013
+parameterstoprog=
+mainfile=tests/unit/integer12
+parameters=
+workingdirectory=/home/simonhj/src/jalangi
+resources=
+analysis=rerunall

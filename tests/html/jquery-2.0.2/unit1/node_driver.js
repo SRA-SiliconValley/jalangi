7abc55b87f37abda0d77a3d8f307d75adc86942f
Manu Sridharan <m.sridharan@samsung.com>
2013-12-11 16:58:02 -0800

    re-organize tests

--- tests/html/jquery-2.0.2/unit1/node_driver.js
+++ tests/html/jquery-2.0.2/unit1/node_driver.js
@@ -2,7 +2,7 @@ var jsdom = require("jsdom");
 var fs = require("fs");
 var jalangi = !!process.argv[2];
 
-var scripts = jalangi ?   ['../../../src/js/analysis.js', '../../../src/js/InputManager.js', '../../../node_modules/escodegen/escodegen.browser.js', '../../../node_modules/esprima/esprima.js', '../../../src/js/utils/astUtil.js', '../../../src/js/instrument/esnstrument.js', 'jquery-2.0.2_jalangi_.js'] : ['jquery-2.0.2.js'];
+var scripts = jalangi ?   ['../../../src/js/analysis.js', '../../../src/js/InputManager.js', '../../../node_modules/escodegen/escodegen.browser.js', '../../../node_modules/esprima/esprima.js', '../../../src/js/utils/astUtil.js', '../../../src/js/instrument/esnstrument.js', '../jquery-2.0.2_jalangi_.js'] : ['../jquery-2.0.2.js'];
 
 jsdom.env(
   '<html><head></head><body></body></html>',

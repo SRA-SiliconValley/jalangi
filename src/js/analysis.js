ddc1a0fa69a8ecb092d06ee37b16a2f7d2c00321
Manu Sridharan <m.sridharan@samsung.com>
2013-12-23 10:49:54 -0800

    print uncaught exception to stderr

--- src/js/analysis.js
+++ src/js/analysis.js
@@ -705,7 +705,7 @@ if (typeof J$ === 'undefined') J$ = {};
                 if ((mode === MODE_REPLAY && scriptCount > 0) || isBrowserReplay) {
                     throw tmp;
                 } else {
-                    console.log(tmp);
+                    process.stderr.write(String(tmp));
                 }
             }
         }

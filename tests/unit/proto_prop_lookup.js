a3970532e1ca102c916342aa430039b2b02dcfbe
ksen <ksen@cs.berkeley.edu>
2014-02-11 15:49:05 -0800

    added a test

---
+++ tests/unit/proto_prop_lookup.js
@@ -0,0 +1,33 @@
+function F() {
+    this.x = 1;
+}
+
+F.prototype.foo = function() {
+    this.x = 2;
+};
+
+var x = new F();
+
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
+x.foo();
